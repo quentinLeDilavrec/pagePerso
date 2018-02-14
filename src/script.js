2162627609bf56d264f5959cf8586b9da35245f1
quentinLeDilavrec <ledilavrecquentin@gmail.com>
2018-02-14 23:59:50 +0100

    rm useless things

--- src/script.js
+++ src/script.js
@@ -20,23 +20,3 @@ function w3_close() {
     mySidebar.style.display = "none";
     overlayBg.style.display = "none";
 }
-
-// jQuery(document).ready(function(){
-// 	if( $('.my-nav').length > 0 ) {
-// 		var stretchyNavs = $('.my-nav');
-		
-// 		stretchyNavs.each(function(){
-// 			var stretchyNav = $(this),
-// 				stretchyNavTrigger = stretchyNav.find('.trigger');
-			
-// 			stretchyNavTrigger.on('click', function(event){
-// 				event.preventDefault();
-// 				stretchyNav.toggleClass('nav-is-visible');
-// 			});
-// 		});
-
-// 		$(document).on('click', function(event){
-// 			( !$(event.target).is('.trigger') && !$(event.target).is('.trigger span') ) && stretchyNavs.removeClass('nav-is-visible');
-// 		});
-// 	}
-// });
